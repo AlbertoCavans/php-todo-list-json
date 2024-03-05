0a7aa74cd319e5d0946b7b055b83e337d7345b20
AlbertoCavans <alberto96cavani@gmail.com>
2024-03-06 00:01:31 +0100

    Feat main.js - array todos

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -3,7 +3,28 @@ const { createApp } = Vue;
 const app = createApp({
   data() {
     return {
-      todolist: [],
+      todos: [
+        {
+          task: "task 1",
+          done: false,
+        },
+        {
+          task: "task 2",
+          done: false,
+        },
+        {
+          task: "task 3",
+          done: true,
+        },
+        {
+          task: "task 4",
+          done: false,
+        },
+        {
+          task: "task 5",
+          done: false,
+        },
+      ],
     };
   },
 });
